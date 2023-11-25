653184d0e356bb62ac1fb268a9cc7ce088efd764
Darren Temporale <tempord@mcmaster.ca>
2023-11-25 14:37:46 -0500

    TsomeGameMechs

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -16,28 +16,28 @@ GameMechs::GameMechs(int boardX, int boardY)
 
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
 
 char GameMechs::getInput()
 {
-
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = 1;
 }
 
 void GameMechs::setInput(char this_input)
