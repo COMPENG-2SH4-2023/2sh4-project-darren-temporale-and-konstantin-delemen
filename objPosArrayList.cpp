78d4155256c39cacf084813a5cdcd211fcd71e6f
delemenk <144150698+delemenk@users.noreply.github.com>
2023-11-29 19:27:26 -0500

    Add files via upload

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -6,61 +6,60 @@
 
 
 objPosArrayList::objPosArrayList() {
-    sizeArray = ARRAY_MAX_CAP;
-    sizeList = 0;
-    aList = new objPos[sizeArray];
+    sizeArray = ARRAY_MAX_CAP; // initialize the size of the array to the maximum capacity
+    sizeList = 0; // initialize the size of the list to zero
+    aList = new objPos[sizeArray]; // dynamically allocate memory for the array of objPos objects
 }
 
 objPosArrayList::~objPosArrayList() {
-    delete[] aList;
+    delete[] aList; // free the memory allocated for the array
 }
 
 int objPosArrayList::getSize() {
-    return sizeList;
+    return sizeList; // return the current size of the list
 }
 
 void objPosArrayList::insertHead(objPos thisPos) {
-    for (int i = sizeList - 1; i >= 0; i--) {
-        aList[i + 1] = aList[i];
+    for (int i = sizeList - 1; i >= 0; i--) { // loop from the end of the list to the beginning
+        aList[i + 1] = aList[i]; // shift each element one position to the right
     }
-    aList[0] = thisPos;
-    sizeList++;
+    aList[0] = thisPos; // insert the given objPos object at the head of the list
+    sizeList++; // increment the size of the list
 }
 
 void objPosArrayList::insertTail(objPos thisPos) {
-    aList[sizeList - 1] = thisPos;
-    sizeList++;
+    aList[sizeList - 1] = thisPos; // insert the given objPos object at the tail of the list
+    sizeList++; // increment the size of the list
 }
 
 void objPosArrayList::removeHead() {
-    for (int i = 1; i < sizeList; i++) {
-        aList[i - 1] = aList[i];
+    for (int i = 1; i < sizeList; i++) { // loop from the second element to the end of the list
+        aList[i - 1] = aList[i]; // shift each element one position to the left
     }
-    removeTail();
+    removeTail(); // remove the last element of the list
 }
 
 void objPosArrayList::removeTail() {
-    aList[sizeList - 1].x = 0;
-    aList[sizeList - 1].y = 0;
-    aList[sizeList - 1].symbol = 0;
-    sizeList--;
+    aList[sizeList - 1].x = 0; // set the x coordinate of the last element to zero
+    aList[sizeList - 1].y = 0; // set the y coordinate of the last element to zero
+    aList[sizeList - 1].symbol = 0; // set the symbol of the last element to zero
+    sizeList--; // decrement the size of the list
 }
 
 void objPosArrayList::getHeadElement(objPos &returnPos) {
-    returnPos.x = aList[0].x;
-    returnPos.y = aList[0].y;
-    returnPos.symbol = aList[0].symbol;
+    returnPos.x = aList[0].x; // copy the x coordinate of the head element to the returnPos object
+    returnPos.y = aList[0].y; // copy the y coordinate of the head element to the returnPos object
+    returnPos.symbol = aList[0].symbol; // copy the symbol of the head element to the returnPos object
 }
 
 void objPosArrayList::getTailElement(objPos &returnPos) {
-    returnPos.x = aList[sizeList - 1].x;
-    returnPos.y = aList[sizeList - 1].y;
-    returnPos.symbol = aList[sizeList - 1].symbol;
+    returnPos.x = aList[sizeList - 1].x; // copy the x coordinate of the tail element to the returnPos object
+    returnPos.y = aList[sizeList - 1].y; // copy the y coordinate of the tail element to the returnPos object
+    returnPos.symbol = aList[sizeList - 1].symbol; // copy the symbol of the tail element to the returnPos object
 }
 
 void objPosArrayList::getElement(objPos &returnPos, int index) {
-    returnPos.x = aList[index].x;
-    returnPos.y = aList[index].y;
-    returnPos.symbol = aList[index].symbol;
+    returnPos.x = aList[index].x; // copy the x coordinate of the element at the given index to the returnPos object
+    returnPos.y = aList[index].y; // copy the y coordinate of the element at the given index to the returnPos object
+    returnPos.symbol = aList[index].symbol; // copy the symbol of the element at the given index to the returnPos object
 }
-
